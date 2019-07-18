8de7bc4cdd285e689eca95b9710dbadc6a306b78
OlhaKa <karazhakovao@gmail.com>
2019-07-18 16:12:18 +0300

    'home'

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".home-banners").slick({
+        slidesToShow: 1,
+        infinite: true,
+        nextArrow: '<i class="fas fa-chevron-right"></i></i>',
+        prevArrow: '<i class="fas fa-chevron-left"></i></i>',
+    });
+
+    $(".home-brands").slick({
+        slidesToShow: 1,
+        infinite: true,
+        nextArrow: '<i class="fas fa-chevron-right"></i></i>',
+        prevArrow: '<i class="fas fa-chevron-left"></i></i>',
+    });
+
+   });
