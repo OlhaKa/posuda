844cd196fd5a02c82ec374cb231ac41f9b0c1ca1
OlhaKa <karazhakovao@gmail.com>
2019-07-19 15:04:19 +0300

    footer, nav menu

--- js/main.js
+++ js/main.js
@@ -12,5 +12,12 @@ $(document).ready(function(){
         nextArrow: '<i class="fas fa-chevron-right"></i></i>',
         prevArrow: '<i class="fas fa-chevron-left"></i></i>',
     });
-
-   });
+    
+    $(window).on("load", "scroll", function () {
+        if ((window.innerWidth) < 992) {
+            var slinky = $('.nav-menu').slinky({
+                title: true
+            });
+        }
+    })
+});
