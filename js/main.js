c06ee01c0a57a8e45c14067767e9418540f6dd13
OlhaKa <karazhakovao@gmail.com>
2019-08-08 18:01:04 +0300

    catalog filters

--- js/main.js
+++ js/main.js
@@ -1,51 +1,81 @@
-$(document).ready(function(){
-    $(".home-banners").slick({
-        slidesToShow: 1,
-        infinite: true,
-        nextArrow: '<i class="fas fa-chevron-right"></i></i>',
-        prevArrow: '<i class="fas fa-chevron-left"></i></i>',
-    });
+$(document).ready(function () {
+        $(".home-banners").slick({
+            slidesToShow: 1,
+            infinite: true,
+            nextArrow: '<i class="fas fa-chevron-right"></i></i>',
+            prevArrow: '<i class="fas fa-chevron-left"></i></i>',
+        });
 
-    $(".home-brands").slick({
-        slidesToShow: 1,
-        infinite: true,
-        nextArrow: '<i class="fas fa-chevron-right"></i></i>',
-        prevArrow: '<i class="fas fa-chevron-left"></i></i>',
-    });
-    
-    // $(window).on("load", "resize", function () {
-    //     if ((window.innerWidth) < 992) {
-    //         var slinky = $('.nav-menu').slinky({
-    //             title: true
-    //         });
-    //     }
-    // })
+        $(".home-brands").slick({
+            slidesToShow: 1,
+            infinite: true,
+            nextArrow: '<i class="fas fa-chevron-right"></i></i>',
+            prevArrow: '<i class="fas fa-chevron-left"></i></i>',
+        });
 
-    var $toggle = $('#toggler-menu');
+        $(window).on("load resize", function (e) {
+            changeSelectTitle()
+        });
 
-    $('#main-nav').hcOffcanvasNav({
-        maxWidth: 992,
-        side: "left",
-        labelClose: 'МЕНЮ',
-        labelBack: 'НАЗАД',
-        customToggle: $toggle,
-      });
-        
+        function changeSelectTitle() {
+            if ($(window).width() <= 768) {
+                var selectpicker = $("#sort-select");
+                selectpicker.selectpicker();
+                selectpicker.selectpicker({title: 'СОРТИРОВАТЬ'}).selectpicker('render');
+            }
+        }
 
-      $(".toggler-search").click(function () {
-        $(".header-search").slideToggle();
-    });
+        var counterBox = $('.counterBox');
 
-    $(".toggler-contacts").click(function () {
-        $(".header-contacts-wrap").slideToggle();
-    });
+        $('.filters-btn').click(function () {
+            $('.filters-wrap').toggleClass('showed');
+            $(this).toggleClass('active');
+            if (counterBox.is(":visible")) {
+                counterBox.fadeOut();
+            } else if ($("input:checked").length && $('.filters-wrap').hasClass('showed')) {
+                counterBox.fadeIn();
+            }
+        })
 
-    $('#sort-select').selectpicker({
-        width: 'fit'
-    });
+        function getCounterPosition() {
+            $('input[type="checkbox"]').click(function () {
 
-    $('.filters-btn').click(function () {
-        $('.filters-wrap').toggleClass('showed');
-    })
+                var counterTop = $(this).offset().top - $(window).scrollTop(),
+                    counterLeft = $('.filters-wrap').offset().left + $('.filters-wrap').width() + 11;
 
-});
+                if (counterBox.is(":hidden")) {
+                    counterBox.fadeIn();
+                }
+                counterBox.css({top: counterTop - 60, left: counterLeft});
+            })
+        }
+
+        getCounterPosition();
+
+
+        var $toggle = $('#toggler-menu');
+
+        $('#main-nav').hcOffcanvasNav({
+            maxWidth: 992,
+            side: "left",
+            labelClose: 'МЕНЮ',
+            labelBack: 'НАЗАД',
+            customToggle: $toggle,
+        });
+
+
+        $(".toggler-search").click(function () {
+            $(".header-search").slideToggle();
+        });
+
+        $(".toggler-contacts").click(function () {
+            $(".header-contacts-wrap").slideToggle();
+        });
+
+        $('#sort-select').selectpicker({
+            width: 'fit'
+        });
+
+
+    }
+);
