9d9d7abcb7823d7a532a17a27770dedff1b61914
OlhaKa <karazhakovao@gmail.com>
2019-08-09 18:04:22 +0300

    basket page

--- js/main.js
+++ js/main.js
@@ -3,97 +3,112 @@ $(window).on('load', function () {
 });
 
 $(document).ready(function () {
-        $(".home-banners").slick({
-            slidesToShow: 1,
-            infinite: true,
-            nextArrow: '<i class="fas fa-chevron-right"></i></i>',
-            prevArrow: '<i class="fas fa-chevron-left"></i></i>',
-        });
+    $(".home-banners").slick({
+        slidesToShow: 1,
+        infinite: true,
+        nextArrow: '<i class="fas fa-chevron-right"></i></i>',
+        prevArrow: '<i class="fas fa-chevron-left"></i></i>',
+    });
 
-        $(".home-brands").slick({
-            slidesToShow: 1,
-            infinite: true,
-            nextArrow: '<i class="fas fa-chevron-right"></i></i>',
-            prevArrow: '<i class="fas fa-chevron-left"></i></i>',
-        });
+    $(".home-brands").slick({
+        slidesToShow: 1,
+        infinite: true,
+        nextArrow: '<i class="fas fa-chevron-right"></i></i>',
+        prevArrow: '<i class="fas fa-chevron-left"></i></i>',
+    });
 
-        $(window).on("load resize", function (e) {
-            changeSelectTitle()
-        });
+    $(window).on("load resize", function (e) {
+        changeSelectTitle()
+    });
 
-        function changeSelectTitle() {
-            if ($(window).width() <= 768) {
-                var selectpicker = $("#sort-select");
-                selectpicker.selectpicker();
-                selectpicker.selectpicker({title: 'СОРТИРОВАТЬ'}).selectpicker('render');
-            }
+    function changeSelectTitle() {
+        if ($(window).width() <= 768) {
+            var selectpicker = $("#sort-select");
+            selectpicker.selectpicker();
+            selectpicker.selectpicker({title: 'СОРТИРОВАТЬ'}).selectpicker('render');
         }
+    }
 
-        var counterBox = $('.counterBox');
+    var counterBox = $('.counterBox');
 
-        $('.filters-btn').click(function () {
-            $('.filters-wrap').toggleClass('showed');
-            $(this).toggleClass('active');
-            if (counterBox.is(":visible")) {
-                counterBox.fadeOut();
-            } else if ($("input:checked").length && $('.filters-wrap').hasClass('showed')) {
+    $('.filters-btn').click(function () {
+        $('.filters-wrap').toggleClass('showed');
+        $(this).toggleClass('active');
+        if (counterBox.is(":visible")) {
+            counterBox.fadeOut();
+        } else if ($("input:checked").length && $('.filters-wrap').hasClass('showed')) {
+            counterBox.fadeIn();
+        }
+    })
+
+    function getCounterPosition() {
+        $('input[type="checkbox"]').click(function () {
+
+            var counterTop = $(this).offset().top - $(window).scrollTop(),
+                counterLeft = $('.filters-wrap').offset().left + $('.filters-wrap').width() + 11;
+
+            if (counterBox.is(":hidden")) {
                 counterBox.fadeIn();
             }
+            counterBox.css({top: counterTop - 60, left: counterLeft});
         })
-
-        function getCounterPosition() {
-            $('input[type="checkbox"]').click(function () {
-
-                var counterTop = $(this).offset().top - $(window).scrollTop(),
-                    counterLeft = $('.filters-wrap').offset().left + $('.filters-wrap').width() + 11;
-
-                if (counterBox.is(":hidden")) {
-                    counterBox.fadeIn();
-                }
-                counterBox.css({top: counterTop - 60, left: counterLeft});
-            })
-        }
-
-        getCounterPosition();
-
-
-        var $toggle = $('#toggler-menu');
-
-        $('#main-nav').hcOffcanvasNav({
-            maxWidth: 992,
-            side: "left",
-            labelClose: 'МЕНЮ',
-            labelBack: 'НАЗАД',
-            customToggle: $toggle,
-        });
-
-
-        $(".toggler-search").click(function () {
-            $(".header-search").slideToggle();
-        });
-
-        $(".toggler-contacts").click(function () {
-            $(".header-contacts-wrap").slideToggle();
-        });
-
-        $('#sort-select').selectpicker({
-            width: 'fit'
-        });
-
-        $('body').on('click', function (e) {
-            const filtersWrap = $('.filters-wrap'),
-                filters = filtersWrap.has(e.target).length === 0,
-                filtersBtnWrap = $('.filters-btn');
-                filtersBtn = filtersBtnWrap.has(e.target).length === 0;
-
-            if (!filtersWrap.is(e.target)
-                && filters
-                && !filtersBtnWrap.is(e.target)
-                && filtersBtn) {
-                filtersWrap.removeClass("showed");
-                filtersBtnWrap.removeClass('active');
-                $('.counterBox').fadeOut()
-            }
-        });
     }
-);
+
+    getCounterPosition();
+
+
+    var $toggle = $('#toggler-menu');
+
+    $('#main-nav').hcOffcanvasNav({
+        maxWidth: 992,
+        side: "left",
+        labelClose: 'МЕНЮ',
+        labelBack: 'НАЗАД',
+        customToggle: $toggle,
+    });
+
+
+    $(".toggler-search").click(function () {
+        $(".header-search").slideToggle();
+    });
+
+    $(".toggler-contacts").click(function () {
+        $(".header-contacts-wrap").slideToggle();
+    });
+
+    $('#sort-select').selectpicker({
+        width: 'fit'
+    });
+
+    $('body').on('click', function (e) {
+        const filtersWrap = $('.filters-wrap'),
+            filters = filtersWrap.has(e.target).length === 0,
+            filtersBtnWrap = $('.filters-btn');
+        filtersBtn = filtersBtnWrap.has(e.target).length === 0;
+
+        if (!filtersWrap.is(e.target)
+            && filters
+            && !filtersBtnWrap.is(e.target)
+            && filtersBtn) {
+            filtersWrap.removeClass("showed");
+            filtersBtnWrap.removeClass('active');
+            $('.counterBox').fadeOut()
+        }
+    });
+
+    // ---- Basket item counter------
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
